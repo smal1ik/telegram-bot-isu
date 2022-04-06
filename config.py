ea9301912ea3e46389e626868f7d4e6f850c064a
kliment <klimmitrofanov.2000@gmail.com>
2022-04-07 00:21:53 +0800

    change bot token

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BOT_TOKEN = "5238627803:AAFS_2jPuw1kvxdCO6XdM1f-3ZMdRU2n4b4"
+BOT_TOKEN = "2124449129:AAFnSmQ6v6n4SpQOLKt_3I7TQJibXeE-Rm0"
 APP_URL = "https://telegram-bot-isu.herokuapp.com/" + BOT_TOKEN
 DB_URI = "postgres://oowdckvrarfvpp:d04e21ed0dbeaa5338dcb848fee88204f6a1ca0ed6e2db495fb127622e5ea04d@ec2-18-214-134-226.compute-1.amazonaws.com:5432/df67ldmlghjvsu"
