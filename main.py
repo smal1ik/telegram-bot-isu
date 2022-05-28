10710cc6d58a4f2c7100e80341a4c01dbdfdf97a
MATVEI <motyha18@gmail.com>
2022-05-28 16:32:31 +0800

    1

--- main.py
+++ main.py
@@ -3,58 +3,135 @@ import telebot
 import logging
 import psycopg2
 from config import *
+import requestToBD
+import re
 from flask import Flask, request
 
 bot = telebot.TeleBot(BOT_TOKEN)
-server = Flask(__name__)
-logger = telebot.logger
-logger.setLevel(logging.DEBUG)
+# server = Flask(__name__)
+# logger = telebot.logger
+# logger.setLevel(logging.DEBUG)
 
 connection = psycopg2.connect(DB_URI, sslmode="require")
 cur = connection.cursor()
 
 username = ""
-start_message = \
-    f"""
-        Привет, {username}.\n
-        Я телеграм бот с расписанием ИГУ.\n
-        Для начала введи номер своей группы в формате 02321-ДБ.\n
-        Я могу выдавать расписание на учебный цикл, неделю или день.\n
-    """
+start_message = "Привет, я телеграм бот с расписанием ИГУ.\n" \
+                "Для начала введи номер своей группы в формате 02321-ДБ.\n" \
+                "Я могу выдавать расписание на учебный цикл, неделю или день.\n"
+
+pointer_group = {}
+inline_id = {}
+
 
 @bot.message_handler(commands=["start"])
 def start(message):
+    global pointer_group
+
     id = message.from_user.id
     username = message.from_user.first_name
+    pointer_group.update({id: 0})
+    print(pointer_group)
     bot.send_message(message.chat.id, start_message)
 
-    cur.execute(f"SELECT telegram_id FROM telegram_user WHERE telegram_id = {id}")
-    result = cur.fetchone()
+    keyboard = telebot.types.InlineKeyboardMarkup(row_width=1)
+    groups = requestToBD.get_group()
+    for number in groups[0:5]:
+        keyboard.add(telebot.types.InlineKeyboardButton(text=number[0], callback_data='group'+str(number)))
+    keyboard.row(telebot.types.InlineKeyboardButton('👉', callback_data='right'))
+    bot.send_message(id, text="Выберите группу:", reply_markup=keyboard)
+    # bot.register_next_step_handler_by_chat_id(id, set_number_gruop)
 
-    if not result:
-        cur.execute("INSERT INTO telegram_user(telegram_id, name) VALUES (%s, %s)", (id, username))
-        connection.commit()
-        connection.close()
+    # cur.execute(f"SELECT telegram_id FROM telegram_user WHERE telegram_id = {id}")
+    # result = cur.fetchone()
+
+    # if not result:
+    #     cur.execute("INSERT INTO telegram_user(telegram_id, name) VALUES (%s, %s)", (id, username))
+    #     connection.commit()
 
 
-@bot.message_handler(func=lambda m: True)
-def set_number_gruop(message):
-    number_group = message.text
-    bot.send_message(message.chat.id, number_group)
+def inline_number_group(user_id, messageid):
+    global pointer_group
+    pointer = pointer_group[user_id]
+    keyboard = telebot.types.InlineKeyboardMarkup(row_width=1)
+    groups = requestToBD.get_group()
+    print(pointer_group)
+
+    if pointer <= 0:
+        pointer_group.update({user_id: 0})
+        for number in groups[pointer:pointer + 5]:
+            keyboard.add(telebot.types.InlineKeyboardButton(text=number[0], callback_data='group'+str(number)))
+        keyboard.row(telebot.types.InlineKeyboardButton('👉', callback_data='right'))
+    elif pointer // 5 >= len(groups) // 5:
+        pointer_group.update({user_id: ((len(groups) // 5) * 5)})
+        for number in groups[pointer:pointer + 5]:
+            keyboard.add(telebot.types.InlineKeyboardButton(text=number[0], callback_data='group'+str(number)))
+        keyboard.row(telebot.types.InlineKeyboardButton('👈', callback_data='left'))
+    else:
+        for number in groups[pointer:pointer + 5]:
+            button = telebot.types.InlineKeyboardButton(text=number[0], callback_data='group-'+str(number))
+            keyboard.add(button)
+        buttonNext = telebot.types.InlineKeyboardButton('👉', callback_data='right')
+        buttonBack = telebot.types.InlineKeyboardButton('👈', callback_data='left')
+        keyboard.row(buttonBack, buttonNext)
+
+    bot.edit_message_reply_markup(chat_id=user_id, message_id=messageid, reply_markup=keyboard)
+
+#Выбор группы
+@bot.callback_query_handler(func=lambda button: 'group' in button.data)
+def process_callback_button1(callback_query):
+    global pointer_group
+    bot.answer_callback_query(callback_query_id=callback_query.id)
+    bot.delete_message(chat_id=callback_query.from_user.id, message_id=callback_query.message.id)
+    pointer_group.pop(callback_query.from_user.id)
+    set_number_gruop(callback_query.data[callback_query.data.find('\'')+1:callback_query.data.rfind('\'')], callback_query.from_user.id)
+
+
+
+
+@bot.callback_query_handler(func=lambda button: button.data == 'right')
+def process_callback_button1(callback_query):
+    global pointer_group
+    pointer_group[callback_query.from_user.id] += 5
+    inline_number_group(callback_query.from_user.id, callback_query.message.id)
+    bot.answer_callback_query(callback_query_id=callback_query.id)
+
+
+@bot.callback_query_handler(func=lambda button: button.data == 'left')
+def process_callback_button1(callback_query):
+    global pointer_group
+    pointer_group[callback_query.from_user.id] -= 5
+    inline_number_group(callback_query.from_user.id, callback_query.message.id)
+    bot.answer_callback_query(callback_query_id=callback_query.id)
+
+
+def set_number_gruop(number_group, user_id):
     print(number_group)
-    print(message.json())
+    if requestToBD.insert_user(user_id, number_group):
+        bot.send_message(user_id, "Окей, я запомнил, твоя группа " + number_group + ".")
+    else:
+        bot.send_message(user_id, "Ты уже есть в базе данных, но можешь изменить информацию о себе.")
+    # if requestToBD.check_group(number_group):
+    #     bot.send_message(message.from_user.id, "Окей, я запомнил, твоя группа " + number_group + ".")
+    # else:
+    #     bot.send_message(message.from_user.id, "Такой группы не существует. Попробуй снова.")
+    #     bot.register_next_step_handler_by_chat_id(message.chat.id, set_number_gruop)
 
 
-@server.route(f"/{BOT_TOKEN}", methods=["POST"])
-def rederict_message():
-    json_string = request.get_data().decode("utf-8")
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return "!", 200
+bot.infinity_polling()
 
 
 
-if __name__ == "__main__":
-    bot.remove_webhook()
-    bot.set_webhook(url=APP_URL)
-    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+# @server.route(f"/{BOT_TOKEN}", methods=["POST"])
+# def rederict_message():
+#     json_string = request.get_data().decode("utf-8")
+#     update = telebot.types.Update.de_json(json_string)
+#     bot.process_new_updates([update])
+#     return "!", 200
+#
+#
+#
+# if __name__ == "__main__":
+#     bot.remove_webhook()
+#     bot.set_webhook(url=APP_URL)
+#     server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
---
+++ requestToBD.py
@@ -0,0 +1,242 @@
+import psycopg2
+import requests
+from datetime import datetime
+
+host = "127.0.0.1"
+user = "postgres"
+password = "356211"
+dbname = "test"
+
+connection = psycopg2.connect(host=host, user=user, password=password, database=dbname)
+url = "http://raspmath.isu.ru/getSchedule"
+data = requests.get(url)
+begin_date_pairs = data.json()[0]["begin_date_pairs"]
+end_date_pairs = data.json()[0]["end_date_pairs"]
+
+def check_group(group):
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT number FROM group_number WHERE number = '{group}'")
+    result = cursor.fetchone()
+    connection.commit()
+    cursor.close()
+    return result
+
+
+def get_group():
+    cursor = connection.cursor()
+    cursor.execute("SELECT number FROM group_number")
+    result = cursor.fetchall()
+    connection.commit()
+    cursor.close()
+    return result
+
+
+def load_descipline():
+    insert_query = "INSERT INTO descipline (name) VALUES "
+    cursor = connection.cursor()
+    cursor.execute("DELETE from descipline")
+    connection.commit()
+    for elem in data.json():
+        subject_name = elem['subject_name']
+        if not f"'{subject_name}'" in insert_query:
+            insert_query += '(\'' + subject_name + '\')' + ', '
+    cursor.execute(insert_query[:-2])
+    connection.commit()
+    cursor.close()
+
+
+def load_teacher():
+    insert_query = "INSERT INTO teacher (fio) VALUES "
+    cursor = connection.cursor()
+    for elem in data.json():
+        FIO = elem['lastname'] + " " + elem['firstname'] + " " + elem['patronymic']
+        if not FIO in insert_query:
+            insert_query += '(\'' + FIO + '\')' + ', '
+    cursor.execute(insert_query[:-2])
+    connection.commit()
+    cursor.close()
+
+
+def load_group():
+    insert_query = "INSERT INTO group_number (number) VALUES "
+    cursor = connection.cursor()
+    for elem in data.json():
+        group_number = elem['group_name']
+        if not group_number in insert_query:
+            insert_query += '(\'' + group_number + '\')' + ', '
+    cursor.execute(insert_query[:-2])
+    connection.commit()
+    cursor.close()
+
+
+def load_audience():
+    insert_query = "INSERT INTO audience (name) VALUES "
+    cursor = connection.cursor()
+    for elem in data.json():
+        audience = elem['class_name']
+        if not audience in insert_query:
+            insert_query += '(\'' + audience + '\')' + ', '
+    cursor.execute(insert_query[:-2])
+    connection.commit()
+    cursor.close()
+
+
+def load_pair_type():
+    insert_query = "INSERT INTO type_pair (name) VALUES "
+    cursor = connection.cursor()
+    for elem in data.json():
+        pair_type = elem['pair_type']
+        if not pair_type in insert_query:
+            insert_query += '(\'' + pair_type + '\')' + ', '
+    cursor.execute(insert_query[:-2])
+    connection.commit()
+    cursor.close()
+
+
+def load_pair_time():
+    insert_query = "INSERT INTO time (start_time, end_time) VALUES "
+    cursor = connection.cursor()
+    for elem in data.json():
+        start_time = elem['pair_start_time']
+        end_time = elem['pair_end_time']
+        if not start_time in insert_query and not end_time in insert_query:
+            insert_query += '(\'' + start_time + '\'' + ", " + '\'' + end_time + '\')' + ', '
+    cursor.execute(insert_query[:-2])
+    connection.commit()
+    cursor.close()
+
+
+def load_week():
+    insert_query = "INSERT INTO week (parity, dayweek) VALUES "
+    cursor = connection.cursor()
+    for elem in data.json():
+        if not elem['week_type']:
+            parity = 0
+        elif elem['week_type'] == 'нижняя':
+            parity = 1
+        elif elem['week_type'] == 'верхняя':
+            parity = 2
+        day = elem['weekday']
+        if not f"('{parity}', '{day}')" in insert_query:
+            insert_query += '(\'' + str(parity) + '\'' + ", " + '\'' + day + '\')' + ', '
+    cursor.execute(insert_query[:-2])
+    connection.commit()
+    cursor.close()
+
+
+def load_schedule():
+    schedule = []
+    cursor = connection.cursor()
+    cursor.execute("DELETE from shedule")
+    for elem in data.json():
+
+        if not elem['week_type']:
+            parity = 0
+        elif elem['week_type'] == 'нижняя':
+            parity = 1
+        elif elem['week_type'] == 'верхняя':
+            parity = 2
+
+        cursor.execute(f"SELECT id FROM group_number WHERE number = '{elem['group_name']}'")
+        group_id = cursor.fetchone()[0]
+        cursor.execute(f"SELECT id FROM time WHERE start_time = '{elem['pair_start_time']}'")
+        time_id = cursor.fetchone()[0]
+        cursor.execute(f"SELECT id FROM type_pair WHERE name = '{elem['pair_type']}'")
+        type_pair_id = cursor.fetchone()[0]
+        cursor.execute(f"SELECT id FROM week WHERE parity = '{parity}' and dayweek = '{elem['weekday']}'")
+        week_id = cursor.fetchone()[0]
+        cursor.execute(f"SELECT id FROM descipline WHERE name = '{elem['subject_name']}'")
+        descipline_id = cursor.fetchone()[0]
+        cursor.execute(f"SELECT id FROM audience WHERE name = '{elem['class_name']}'")
+        audience_id = cursor.fetchone()[0]
+        cursor.execute(f"SELECT id FROM teacher WHERE fio = '{elem['lastname']} {elem['firstname']} {elem['patronymic']}'")
+        teacher_id = cursor.fetchone()[0]
+        schedule.append((group_id, time_id, type_pair_id, week_id, descipline_id, audience_id, teacher_id))
+
+    insert_query = "INSERT INTO shedule (group_number_id, time_id, type_pair_id, week_id, descipline_id, audience_id, teacher_id) VALUES (%s,%s,%s,%s,%s,%s,%s)"
+    cursor.executemany(insert_query, schedule)
+    connection.commit()
+    cursor.close()
+
+def insert_user(user_id, number_group):
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT id FROM user_telegram WHERE id = '{user_id}'")
+    if not cursor.fetchone():
+        cursor.execute(f"SELECT id FROM group_number WHERE number = '{number_group}'")
+        group_id = cursor.fetchone()[0]
+        cursor.execute(f"INSERT INTO user_telegram (id, group_number_id) VALUES ({user_id}, {group_id})")
+        connection.commit()
+        cursor.close()
+        return True
+    else:
+        cursor.close()
+        return False
+
+
+
+
+
+def get_schedule(group_number_id, week_day):
+    cursor = connection.cursor()
+
+    cursor.execute(f"SELECT id, parity FROM week WHERE dayweek = '{week_day}' ")
+    days = cursor.fetchall()
+
+    cursor.execute(f"SELECT start_time FROM time")
+    schedule = []
+    # for time in sorted(cursor.fetchall()):
+        # schedule[time[0]] = []
+
+    cursor.execute(f"SELECT number FROM group_number WHERE id = '{group_number_id}'")
+    print("Расписание для группы: " + cursor.fetchone()[0])
+
+    for day in days:
+        cursor.execute(f"SELECT group_number_id, time_id, descipline_id FROM shedule WHERE group_number_id = '{group_number_id}' and week_id = '{day[0]}'")
+
+        for elem in cursor.fetchall():
+            cursor.execute(f"SELECT name FROM descipline WHERE id = '{elem[2]}'")
+            descipline = cursor.fetchone()[0]
+            cursor.execute(f"SELECT start_time, end_time FROM time WHERE id = '{elem[1]}'")
+            time = cursor.fetchone()
+            schedule.append([descipline, time[0], time[1]])
+
+    cursor.close()
+    schedule.sort(key=lambda x: x[2])
+    print(schedule)
+
+day = {0: 'Понедельник', 1: 'Вторник', 2: 'Среда', 3: 'Четверг', 4: 'Пятница', 5: 'Суббота', 6: 'Воскресенье'}
+
+def get_parity(date):
+    d = date
+    d1 = datetime.strptime(begin_date_pairs, "%Y-%m-%d")
+    return (2 - (d - d1).days // 7 % 2)
+
+print(get_parity(datetime.strptime("2022-10-17", "%Y-%m-%d")))
+get_schedule('10', 'Пятница')
+
+# load_descipline()
+# load_schedule()
+# load_teacher()
+# load_group()
+# load_audience()
+# load_pair_type()
+# load_pair_time()
+# load_week()
+# load_schedule()
+
+##{
+#   'id': 1,
+#   'group_name': '02121-ДБ',
+#   'weekday': 'Понедельник',
+#   'pair_start_time': '10.10',
+#   'pair_end_time': '11.40',
+#   'subject_name': 'Иностранный язык',
+#   'pair_type': 'практика',
+#   'lastname': 'Петрова',
+#   'firstname': 'Наталья',
+#   'patronymic': 'Васильевна',
+#   'class_name': '222',
+#   'week_type': '',
+#   'begin_date_pairs': '2022-02-07',
+#   'end_date_pairs': '2022-06-11'
+# }
